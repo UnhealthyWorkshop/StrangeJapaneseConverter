ce8dec0b296bb35181d1abcee09051e780ee396d
DaisenK <b1017150@fun.ac.jp>
2020-11-27 21:08:35 +0900

    [add]StrangeJapaneseConverter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,37 @@
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.File
+
 class Main {
     companion object {
+        lateinit var rJson: String
+        lateinit var stMap: HashMap<String, String>
+
         @JvmStatic
         fun main(args: Array<String>){
-            println("aaa")
+            rJson = File("./src/main/resources/StrangeTable.json").readText()
+
+            //println(convert(args[0]))
+            println(convert("怪しい"))
+        }
+
+        fun convert(str: String): String {
+            val buffer = StringBuffer()
+
+            stMap = jacksonObjectMapper().readValue<StrangeTable>(rJson).list
+
+            for (c in str) {
+                val match = stMap[c.toString()]
+                if (match != null) {
+                    buffer.append(match)
+                } else {
+                    buffer.append(c)
+                }
+            }
+
+            return buffer.toString()
         }
     }
 }
+
+data class StrangeTable(val list: HashMap<String, String>)
---
+++ src/main/kotlin/StrangeJapaneseConv.kt
@@ -0,0 +1,7 @@
+class StrangeJapaneseConv {
+    val w_list: HashMap<String, String> = HashMap()
+
+    init {
+        w_list["し"] = "レ"
+    }
+}
