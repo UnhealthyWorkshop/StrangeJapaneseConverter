bc680fe2364d0174e2cc70f99b9d157f313f7f3e
DaisenK <b1017150@fun.ac.jp>
2020-11-28 00:01:03 +0900

    [mod]StrangeJapaneseConverterのリファクタリング・JavaDocの追加

--- src/main/kotlin/StrangeJapaneseConverter.kt
+++ src/main/kotlin/StrangeJapaneseConverter.kt
@@ -3,33 +3,47 @@ import java.io.File
 import java.io.IOException
 import kotlin.IllegalStateException
 
+/**
+ * 怪しい日本語変換のキモ
+ *
+ * 辞書としてJSONを読み込み、convert()で変換できます。
+ *
+ * JSON例
+ * [
+ *   {"before":"し", "after":"レ"},
+ *   {"before":"語", "after":"语"}
+ * ]
+ */
 class StrangeJapaneseConverter: Throwable() {
-    private lateinit var rJson: String
-    private var stMap: HashMap<String, String> = HashMap()
+    private lateinit var rJson: String //変換前のJSONテキストを入れる
+    private var stMap: HashMap<String, String> = HashMap() //変換のリスト
 
     init {
+        /*ファイルを読み込む*/
         try {
             this.rJson = File("src/main/resources/StrangeTable.json").readText()
-        } catch (e: IOException) {
-            e.stackTrace
+        } catch (e: IOException) {//ファイルが読めないときこれが呼ばれる
+            System.err.println(e.stackTrace)
         }
 
-        jacksonObjectMapper().readValue(rJson, ArrayList<HashMap<String, String>>().javaClass)?.forEach { it ->
+        /*JSONのパース*/
+        jacksonObjectMapper().readValue(rJson, ArrayList<HashMap<String, String>>().javaClass)?.forEach {
             this.stMap[it["before"] ?: throw IllegalStateException("不正なStrangeTable")] = it["after"] ?: throw IllegalStateException("不正なStrangeTable")
         }
-
     }
 
+    /**
+     * 入力文を怪しい日本語に変換します。
+     *
+     * @param str: String 変換元の文
+     * @return String 変換後の文
+     *
+     */
     fun convert(str: String): String {
         val buffer = StringBuffer()
 
-        for (c in str) {
-            val match = this.stMap[c.toString()]
-            if (match != null) {
-                buffer.append(match)
-            } else {
-                buffer.append(c)
-            }
+        str.toMutableList().forEach {
+            buffer.append(stMap[it.toString()] ?: it)
         }
 
         return buffer.toString()
