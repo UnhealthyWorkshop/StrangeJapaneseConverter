5d1bea19650a4938763009c6bf0e47a2d286c749
Tatsuo <48282855+t4t5u0@users.noreply.github.com>
2020-11-28 23:17:16 +0900

    Revert "Feature/mod make json"

--- src/main/python/make_json.py
+++ src/main/python/make_json.py
@@ -16,18 +16,14 @@ def write_json(path: Path, obj: dict) -> None:
         f.writelines(ls)
 
 def main():
-    path = Path.cwd()/'src/main/resources/StrangeTable.json'
-    s1, s2 = input('before_str after_str: ').split()
+    path = Path.cwd()/"src/main/resources/StrangeTable.json"
+    s1, s2 = input("before_str after_str: ").split()
 
-    if s1 == s2:
-        print('Err: 変換前文字列と変換後文字列が同一です．')
-        return 
-
-    if isinstance(s1, str) and isinstance(s2, str):
-        dict_obj = {'before':s1, 'after':s2}
-        write_json(path, dict_obj)
-        return
-    print('Err: 入力形式が不正です．str str の形式で入力してください')
+    if (len(s1) == len(s2)) != 1:
+        print("str str の形式で入力してください")
+        exit(0)
+    dict_obj = {"before":s1, "after":s2}
+    write_json(path, dict_obj)
 
 
 if __name__ == '__main__':
